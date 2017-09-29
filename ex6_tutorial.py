d8124477aed1a849557547b882292c0a22bbcc16
janicelove <jlove5@nd.edu>
2017-09-29 13:23:43 -0400

    was trying out some for loops that did not work, deleted.

--- ex6_tutorial.py
+++ ex6_tutorial.py
@@ -15,10 +15,32 @@ game = numpy.zeros((50,3))
 
 UW_score = 0
 MSU_score = 0
-for i in range (0, len(data),1):
-    if data.team[i] == 'UW':
-        UW_score = UW_score + data.score[i]
-    elif data.team[i] == 'MSU':
-        MSU_score = MSU_score + data.score[i]
 
-    
+
+for i in range (0, len(data),1): #for loop generates final score for each team 
+        if data.team[i] == 'UW':
+            UW_score = UW_score + data.score[i]
+        elif data.team[i] == 'MSU':
+            MSU_score = MSU_score + data.score[i]
+
+
+D = pandas.DataFrame(game, columns[= ['time', 'team', 'score']) 
+
+
+for i in range (0,len(data),1):
+    D.time[i] == D.time[i]
+    if data.team[i]== 'UW':
+        D.team[i] = 'UW'
+    elif data.team[i] == 'MSU':
+        D.team[i] = 'MSU'
+
+        
+for i in range (0,len(data),1):
+    if i == 0:
+        D.score[i] = data.score[i] 
+    elif i > 0: 
+        D.score[i] = data.score[i] + D.score[i-1]
+
+
+
+plot = plt.plot(data.time,UWscore,'r-',data.time,MSUscore,'g-')    
