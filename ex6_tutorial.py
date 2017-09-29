af24b713a307522fa8812d85888fd21d97c65e5e
janicelove <jlove5@nd.edu>
2017-09-29 11:19:35 -0400

    for loop to generate total scores of teams

--- ex6_tutorial.py
+++ ex6_tutorial.py
@@ -13,6 +13,12 @@ data = pandas.read_csv("UWvMSU_1-22-13.txt", delimiter = '\t')
 
 game = numpy.zeros((50,3))
 
+UW_score = 0
+MSU_score = 0
+for i in range (0, len(data),1):
+    if data.team[i] == 'UW':
+        UW_score = UW_score + data.score[i]
+    elif data.team[i] == 'MSU':
+        MSU_score = MSU_score + data.score[i]
 
-for i in data:
     
