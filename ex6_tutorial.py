433df83312405cc922e0b4ab3ba5e3c914c1bf45
janicelove <jlove5@nd.edu>
2017-09-29 10:49:29 -0400

    file for script for exercise 6

---
+++ ex6_tutorial.py
@@ -0,0 +1,10 @@
+#script for Exercise 6: using control structures in python 
+#Authors: Melissa Stephens and Janice Love 
+#Date: 9-29-2017
+
+f = open('UWvMSU_1-22-13.txt' ,'r')
+print f 
+
+import pandas
+import matplotlib.pyplot as plt 
+
