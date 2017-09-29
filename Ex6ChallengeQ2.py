c7bf59987704c3575fc101acf7c2a8e01b75a845
Grant Keller <gkeller1@nd.edu>
2017-09-29 12:16:17 -0400

    Collaborated in class - Q2 working. Needs comments, maybe design a GUI for the game?

--- Ex6ChallengeQ2.py
+++ Ex6ChallengeQ2.py
@@ -1,1 +1,15 @@
 # Using control structures in Python
+
+import numpy as np
+
+
+computer_number = np.random.randint(101)
+user_number = -10
+while user_number != computer_number50:  
+    user_number = int(raw_input("Guess a number between 1 and 100: "))
+    if user_number > computer_number:
+        print("TOO HIGH NERD")
+    elif user_number < computer_number:
+        print("That's way too low.")
+        
+print("You won the game!")
