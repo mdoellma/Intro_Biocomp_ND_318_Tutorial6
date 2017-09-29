26d3b1cd62db1bda73ca229702b8c3529ec87e39
Om Neelay <oneelay@nd.edu>
2017-09-29 10:54:24 -0400

    Import and set wd and read text file

---
+++ Exercise6.py
@@ -0,0 +1,14 @@
+#Import Stuff
+import os
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+
+#Set Om's WD
+os.chdir('/Users/omneelay/Desktop/Exercise6/Intro_Biocomp_ND_318_Tutorial6/')
+
+#Import Data
+scores=pandas.read_csv("UWvMSU_1-22-13.txt", sep='\t')
+
+
+
