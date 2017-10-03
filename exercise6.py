3b27b18c34f35df82c60c5802bd82169deb0dce2
Devin Layne Shirley <devin.layne.shirley@gmail.com>
2017-10-03 12:24:25 -0400

    Two versions of the guess my number game

--- exercise6.py
+++ exercise6.py
@@ -1,45 +1,34 @@
 #change to appropriate directory with unix commands or with 'preferences'
 #1 plot generation
-#add data
-
-#add packages
-import matplot.pyplot as plt
 
 
-#2 game (guess my number)
+
+#2 GAME: GUESS MY Number Vs 1
 #add packages
 import numpy as np
+#select code from here to break to run game
 #make array from 1:100
 numbers=range(1,101,1)
 #computer assigns random numbers
 random=np.random.choice(numbers)
-guess= raw_input("I'm thinking of a number between 1-100. Try to guess it:")
-attempt= 0
-while guess != random and attempt < random:
-    print("Higher")
-    guess=raw_input("Guess: ")
-    attempt= guess
-if attempt==random:
-    print("Correct!")
-elif attempt < random:
-    print("Higher")
-else:
-    print("Lower")
-#check http://www.openbookproject.net/books/bpp4awd/ch04.html
-#or https://wiki.python.org/moin/WhileLoop
+#set number outside of range so that the first loop works
+guess=0
+print("I'm thinking of a number between 1-100. Try to guess it.")
+while guess != random:
+    guess=raw_input('Guess:') #gives prompt
+    guess=int(guess) #converts guess to integer
+    if guess < random: #if guess is too low
+        print('Guess:' + str(guess))
+        print("Higher")
+    elif guess > random: #if guess is too high
+       print('Guess:'+ str(guess))
+       print("Lower")
+    else: #if guess is correct
+        print('Guess:'+ str(guess))
+        print("Correct! " + str(guess))
+        break #ends the loop
 
-##attempt # 2
-random=np.random.choice(numbers)
-guess= raw_input("I'm thinking of a number between 1-100. Try to guess it:")
-while guess != random and guess < random:
-    print("Higher")
-    guess= raw_input("Guess: ")
-elif guess > random:
-    print("Higher")
-    guess= raw_input("Guess: ")
-else:
-    print("Correct!)
-    
+#GAME Guess my Number Vers. 2.0 (also asks name and says number of guesses)
 #select all of the following text and run it 
 #add packages 
 import numpy as np
@@ -61,15 +50,15 @@ while guessesTaken < 100: #runs for 100 guesses, then will terminate
     guessesTaken = guessesTaken + 1 #adds one for each guess
     
     if guess < random: #user's guess too low
-        print('Your guess is too low.') 
+        print('Your guess, ' + str(guess) + ', is too low.') 
 
     if guess > random: #user's guess too high
-        print('Your guess is too high.')
+        print('Your guess, ' + str(guess) + ', is too high.')
 
     if guess == random: #user's guess correct
         break
 if guess == random: #once correct it lets the user know they were correct
     guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
-
+    print('Good job, ' + myName + '! You guessed my number, ' + str(guess) + ', in ' + guessesTaken + ' guesses!')
+#game complete at this point
 
