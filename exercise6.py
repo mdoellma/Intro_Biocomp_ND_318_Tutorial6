285ec09a3b0fa8882974ac2c6038c41bea5768ef
Devin Layne Shirley <devin.layne.shirley@gmail.com>
2017-10-03 10:06:00 -0400

    game works now, needs cleaning though

--- exercise6.py
+++ exercise6.py
@@ -15,7 +15,7 @@ numbers=range(1,101,1)
 random=np.random.choice(numbers)
 guess= raw_input("I'm thinking of a number between 1-100. Try to guess it:")
 attempt= 0
-while guess != random and guess < random:
+while guess != random and attempt < random:
     print("Higher")
     guess=raw_input("Guess: ")
     attempt= guess
@@ -29,4 +29,47 @@ else:
 #or https://wiki.python.org/moin/WhileLoop
 
 ##attempt # 2
-#
+random=np.random.choice(numbers)
+guess= raw_input("I'm thinking of a number between 1-100. Try to guess it:")
+while guess != random and guess < random:
+    print("Higher")
+    guess= raw_input("Guess: ")
+elif guess > random:
+    print("Higher")
+    guess= raw_input("Guess: ")
+else:
+    print("Correct!)
+    
+#select all of the following text and run it 
+#add packages 
+import numpy as np
+#how many guesses taken, will be added to
+guessesTaken = 0
+#make array from 1:100
+numbers=range(1,101,1)
+#computer assigns random numbers
+random=np.random.choice(numbers)
+#program asks user name
+print('Hello! What is your name?')
+myName = raw_input() #user supplies name
+print('Well, ' + myName + ', I am thinking of a number between 1 and 100.')#let the games begin
+while guessesTaken < 100: #runs for 100 guesses, then will terminate
+    print('Take a guess.') 
+    guess = raw_input() #takes user's first guess
+    guess = int(guess)
+    
+    guessesTaken = guessesTaken + 1 #adds one for each guess
+    
+    if guess < random: #user's guess too low
+        print('Your guess is too low.') 
+
+    if guess > random: #user's guess too high
+        print('Your guess is too high.')
+
+    if guess == random: #user's guess correct
+        break
+if guess == random: #once correct it lets the user know they were correct
+    guessesTaken = str(guessesTaken)
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+
+
