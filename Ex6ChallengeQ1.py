c4fa519dcd4e63b6e201d18f09aa8948adb17970
Grant Keller <gkeller1@nd.edu>
2017-10-05 18:03:55 -0400

    Cleaned up comments, finalized docstring. Great job on the plot Kathleen!

--- Ex6ChallengeQ1.py
+++ Ex6ChallengeQ1.py
@@ -1,28 +1,21 @@
-# Using control structures in Python
-# Authors: Grant Keller and Kathleen Nicholson
 """
-TO-DO:
-
-1. Label x axis. --DONE
-2. Label y axis. --DONE
-3. Include legend. --DONE
-4. Title the plot. --DONE
-5. Maybe add gridlines? This could make it more informative. --DONE
-6. Comment this code after (see Q2 for examples). --DONE
+Makes a fun and informative plot from the score/time info provided for us.
+UW vs. MSU, 01/22/13.
+Authors: Grant Keller and Kathleen Nicholson
 """
 
 
 import pandas
 import matplotlib.pyplot as plt
-#Q1
-#Creates UWvMSU csv as a variable.
+# Q1
+# Reads in UWvMSU txt file as pandas dataframe
 UWvMSU = pandas.read_csv('UWvMSU_1-22-13.txt', sep='\t')
-#This sets the scores of each team and time as variables equal to zero to begin.
+# Instantiates list to hold time & score variables, sets first point at 0.
 UW_score = [0]
 MSU_score = [0]
 time = [0]
-#This for loop runs through each row and appends zero to the nonscoring team's cumulative score and appends the points earned to the scoring team's
-#     cumulative score
+# Runs through each row and appends zero to the nonscoring team's cumulative score
+#   and appends points earned to the scoring team's cumulative score
 for i in range(0, UWvMSU.shape[0], 1):
     if UWvMSU.team[i] == 'UW':
         UW_score.append(UW_score[-1]+UWvMSU.score[i])
@@ -31,15 +24,15 @@ for i in range(0, UWvMSU.shape[0], 1):
         MSU_score.append(MSU_score[-1]+UWvMSU.score[i])
         UW_score.append(UW_score[-1])
     time.append(UWvMSU.time[i])
-#This plots the cumulative scores as they are added over time and makes the UW score the red line and the MSU score the green line.
+# Plots the cumulative scores as they are added over time and makes the UW score a red line and the MSU score a green line.
 plt.plot(time, UW_score, 'r-', time, MSU_score, 'g-')
-#This adds the figure legend with corresponding colors. 
+# Adds the figure legend with corresponding colors. 
 plt.legend(('UW', 'MSU',))
-#This titles the plot and gives the x and y axis labels. 
+# Titles the plot, gives the x and y axis labels. 
 plt.title("UW vs. MSU Game Score")
 plt.xlabel("time (s)")
 plt.ylabel("score")
-#This adds gridlines to the plot
+# Adds gridlines to the plot
 plt.grid(True)
 
 
